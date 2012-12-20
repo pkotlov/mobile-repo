f7de9ea85bb1f6b535dadc14fe0625e1f49e72ec
pkotlov <pkotlov@gmail.com>
2012-12-20 02:12:51 +0100

    test commit

--- mobile-search/src/de/aiag/mobile/search/MyPhoneGapActivity.java
+++ mobile-search/src/de/aiag/mobile/search/MyPhoneGapActivity.java
@@ -3,10 +3,19 @@ package de.aiag.mobile.search;
 import com.phonegap.*;
 import android.os.Bundle;
 
+/**
+ * @author pkotlov
+ *
+ */
 public class MyPhoneGapActivity extends DroidGap {
+
+	/* (non-Javadoc)
+	 * @see com.phonegap.DroidGap#onCreate(android.os.Bundle)
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.loadUrl("file:///android_asset/www/index.html");
 	}
+
 }
